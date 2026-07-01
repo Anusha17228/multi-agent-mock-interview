4091054547b27e659dfd0456216bbb94137ce27c
Anusha R <anuravipriya7@gmail.com>
2026-07-01 10:53:42 +0530

    Update session_manager.py

--- services/session_manager.py
+++ services/session_manager.py
@@ -1,1 +1,16 @@
+import json
+import os
 
+SESSION_FILE = "data/sessions.json"
+
+def save_session(data):
+    sessions = []
+
+    if os.path.exists(SESSION_FILE):
+        with open(SESSION_FILE, "r") as file:
+            sessions = json.load(file)
+
+    sessions.append(data)
+
+    with open(SESSION_FILE, "w") as file:
+        json.dump(sessions, file, indent=4)
