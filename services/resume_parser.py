351d8ecca30838e1d560234fc2cae3448aa45814
Anusha R <anuravipriya7@gmail.com>
2026-07-01 11:59:35 +0530

    Update resume_parser.py

--- services/resume_parser.py
+++ services/resume_parser.py
@@ -1,2 +1,25 @@
+from pypdf import PdfReader
+import io
+
+
 def parse_resume(uploaded_file):
-    return uploaded_file.read().decode("utf-8")
+    file_type = uploaded_file.name.split(".")[-1].lower()
+
+    # Handle TXT files
+    if file_type == "txt":
+        return uploaded_file.read().decode("utf-8")
+
+    # Handle PDF files
+    elif file_type == "pdf":
+        pdf_reader = PdfReader(io.BytesIO(uploaded_file.read()))
+        text = ""
+
+        for page in pdf_reader.pages:
+            extracted = page.extract_text()
+            if extracted:
+                text += extracted + "\n"
+
+        return text
+
+    else:
+        raise ValueError("Unsupported file format. Upload TXT or PDF.")
